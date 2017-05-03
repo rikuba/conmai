8e6641840c2b23c85b9fc326bd5d9b626eabbaf7
rikuba <rikuba@live.jp>
2017-05-03 12:40:30 +0900

    Refactoring configureStore

---
+++ src/renderer/components/toolbar/toolbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <div className="toolbar">
+    <button>字幕</button>
+    <input type="text" />
+    <button>取得</button>
+    <button>更新</button>
+  </div>
+);
--- src/renderer/store/index.ts
+++ src/renderer/store/index.ts
@@ -4,7 +4,10 @@ import rootReducer from '../reducers';
 
 export default function configureStore() {
   if (process.env.NODE_ENV === 'development') {
-    return createStore(rootReducer, window['__REDUX_DEVTOOLS_EXTENSION__']());
+    return createStore(
+      rootReducer,
+      window['__REDUX_DEVTOOLS_EXTENSION__'] && window['__REDUX_DEVTOOLS_EXTENSION__']()
+    );
   }
 
   return createStore(rootReducer);
