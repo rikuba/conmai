637a6acee1a5c4df354e34651fb02a6c560fccd2
rikuba <rikuba@live.jp>
2017-05-03 09:22:44 +0900

    Prepare to use redux

---
+++ src/renderer/store/index.ts
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+
+import rootReducer from '../reducers';
+
+export default function configureStore() {
+  if (process.env.NODE_ENV === 'development') {
+    return createStore(rootReducer, window['__REDUX_DEVTOOLS_EXTENSION__']());
+  }
+
+  return createStore(rootReducer);
+}
