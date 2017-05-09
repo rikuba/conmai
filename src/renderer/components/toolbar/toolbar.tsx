0fbea1d022b0a6a9d1457672c901845d22cce275
rikuba <rikuba@live.jp>
2017-05-09 17:48:02 +0900

    feat(toolbar): URL入力欄でEscapeキーを押したとき、URLを元に戻す

--- src/renderer/components/toolbar/toolbar.tsx
+++ src/renderer/components/toolbar/toolbar.tsx
@@ -3,23 +3,40 @@ import React from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 
-import { State } from '../../reducers';
+import { State, Thread, getSelectedThread } from '../../reducers';
 import { openThread, updateSelectedThread } from '../../actions';
 
 import './toolbar.css';
+
+interface StateProps {
+  selectedThread: Thread;
+}
 
 interface DispatchProps {
   openThread: (url: string) => Promise<void>;
   updateSelectedThread: () => Promise<void>;
 }
 
+const mapStateToProps = (state: State) => ({
+  selectedThread: getSelectedThread(state),
+});
+
 const mapDispatchToProps = (dispatch: Dispatch<State>): DispatchProps => ({
   openThread: (url: string) => dispatch(openThread(url)),
   updateSelectedThread: () => dispatch(updateSelectedThread()),
 });
 
-class ToolbarComponent extends React.Component<DispatchProps, any> {
+class ToolbarComponent extends React.Component<StateProps & DispatchProps, any> {
   private urlInput: HTMLInputElement;
+
+  handleUrlInputKeydown = (e: any) => {
+    if (e.key === 'Escape') {
+      const { selectedThread } = this.props;
+      if (selectedThread) {
+        this.urlInput.value = selectedThread.url;
+      }
+    }
+  };
 
   handleUrlSubmit = (e: any) => {
     e.preventDefault();
@@ -33,13 +50,17 @@ class ToolbarComponent extends React.Component<DispatchProps, any> {
   };
 
   render() {
+    const { selectedThread } = this.props;
+    const url = selectedThread ? selectedThread.url : '';
+
     return (
       <div className="toolbar">
         <button onClick={(e) => ipcRenderer.send('open-subwindow-request')}>字幕</button>
         <form onSubmit={this.handleUrlSubmit} className="url-form">
           <input type="text" className="url-input"
             placeholder="URLを入力します"
-            defaultValue="http://jbbs.shitaraba.net/bbs/read.cgi/computer/42660/1462262759/"
+            defaultValue={url}
+            onKeyDown={this.handleUrlInputKeydown}
             ref={(node) => this.urlInput = node} />
           <button type="submit">開く</button>
         </form>
@@ -49,4 +70,4 @@ class ToolbarComponent extends React.Component<DispatchProps, any> {
   }
 }
 
-export default connect(null, mapDispatchToProps)(ToolbarComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(ToolbarComponent);
