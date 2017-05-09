dea0eacd8718f2da7057c225d176efa385d9c8d2
rikuba <rikuba@live.jp>
2017-05-10 02:58:46 +0900

    feat(thread): 更新完了時に最後のpostまでスクロールする

--- src/renderer/components/thread/thread.tsx
+++ src/renderer/components/thread/thread.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import PostComponent from '../post/post';
 import { Thread } from '../../reducers';
@@ -6,6 +7,13 @@ import { Thread } from '../../reducers';
 import './thread.css';
 
 export default class ThreadComponent extends React.Component<Thread, any> {
+  componentDidUpdate() {
+    const elm = ReactDOM.findDOMNode(this);
+    if (elm) {
+      elm.scrollTop = elm.scrollHeight - elm.clientHeight;
+    }
+  }
+
   render() {
     const { posts } = this.props;
 
