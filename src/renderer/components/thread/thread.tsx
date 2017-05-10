0ec673fb5670d02633df6bfd829b0ab8d1d0b35c
rikuba <rikuba@live.jp>
2017-05-10 16:04:59 +0900

    fix(thread): 更新完了時に新着レスがなければスクロールしない

--- src/renderer/components/thread/thread.tsx
+++ src/renderer/components/thread/thread.tsx
@@ -7,14 +7,20 @@ import { Thread } from '../../reducers';
 import './thread.css';
 
 export default class ThreadComponent extends React.Component<Thread, any> {
+  private lastNewPostNumber = 0;
+
   componentDidUpdate() {
     const { newPostNumber } = this.props;
-    if (newPostNumber) {
-      const newPost = ReactDOM.findDOMNode(this)
-        .querySelector(`.post[data-number="${newPostNumber}"]`);
-      if (newPost) {
-        newPost.scrollIntoView();
-      }
+    if (newPostNumber !== this.lastNewPostNumber) {
+      this.scrollToNewPost();
+      this.lastNewPostNumber = newPostNumber;
+    }
+  }
+
+  scrollToNewPost() {
+    const newPost = ReactDOM.findDOMNode(this).querySelector('.post.new');
+    if (newPost) {
+      newPost.scrollIntoView();
     }
   }
 
