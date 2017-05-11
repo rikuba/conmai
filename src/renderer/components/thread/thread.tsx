0c58cbf878cdb9dca4700a61906dcce16000e3fd
rikuba <rikuba@live.jp>
2017-05-11 16:57:03 +0900

    fix(thread): 末尾までスクロールされている場合にのみ新規投稿へ自動スクロールする

--- src/renderer/components/thread/thread.tsx
+++ src/renderer/components/thread/thread.tsx
@@ -8,11 +8,16 @@ import './thread.css';
 
 export default class ThreadComponent extends React.Component<Thread, any> {
   private lastNewPostNumber = 0;
+  private isScrolledToTheEnd = true;
+
+  constructor(props: Thread) {
+    super(props);
+  }
 
   componentDidUpdate() {
     const { newPostNumber } = this.props;
     if (newPostNumber !== this.lastNewPostNumber) {
-      if (newPostNumber > 1) {
+      if (newPostNumber > 1 && this.isScrolledToTheEnd) {
         this.scrollToNewPost();
       }
       this.lastNewPostNumber = newPostNumber;
@@ -20,18 +25,21 @@ export default class ThreadComponent extends React.Component<Thread, any> {
   }
 
   scrollToNewPost() {
-    const newPost = ReactDOM.findDOMNode(this).querySelector('.post[data-is-new="true"]');
-    if (newPost) {
-      newPost.scrollIntoView();
-    }
+    const elm = ReactDOM.findDOMNode(this);
+    elm.scrollTop = elm.scrollHeight - elm.clientHeight;
   }
+
+  handleScroll = (e: any) => {
+    const elm: HTMLDivElement = e.currentTarget;
+    this.isScrolledToTheEnd = elm.scrollTop >= elm.scrollHeight - elm.clientHeight - 10;
+  };
 
   render() {
     const { posts, newPostNumber } = this.props;
     const isNew = (number: number) => number >= newPostNumber;
 
     return (
-      <div className="thread">
+      <div className="thread" onScroll={this.handleScroll}>
         {posts.map((post) => (
           <PostComponent key={post.number} isNew={isNew(post.number)} {...post} />
         ))}
