2f1443bb1e882391197869f09a51c9a89a62eb41
rikuba <rikuba@live.jp>
2017-05-20 21:35:01 +0900

    feat: 字幕ウィンドウ上のコンテキストメニューを実装する

--- src/renderer/pages/sub.tsx
+++ src/renderer/pages/sub.tsx
@@ -1,9 +1,25 @@
-import { ipcRenderer } from 'electron';
+import { ipcRenderer, remote } from 'electron';
 import debounce from 'lodash.debounce';
 
 import { Post } from '../reducers';
 
 import './sub.css';
+
+const myCommentContextMenu = remote.Menu.buildFromTemplate([
+  { role: 'undo', label: '元に戻す' },
+  { type: 'separator' },
+  { role: 'cut', label: '切り取り' },
+  { role: 'copy', label: 'コピー' },
+  { role: 'paste', label: '貼り付け' },
+  { role: 'delete', label: '削除' },
+  { type: 'separator' },
+  { role: 'selectall', label: 'すべて選択' },
+]);
+
+const postCommentContextMenu = remote.Menu.buildFromTemplate([
+  { role: 'copy', label: 'コピー' },
+  { role: 'selectall', label: 'すべて選択' },
+]);
 
 const posts: Post[] = [];
 let myComment = localStorage.getItem('my-comment') || '';
@@ -29,13 +45,27 @@ function showPost() {
   timerId = setTimeout(showPost, 10 * 1000 / (posts.length + 1));
 }
 
-const myCommentElement = document.querySelector('.my-comment') as HTMLDivElement;
+const myCommentElement = document.querySelector('.my-comment') as HTMLParagraphElement;
 myCommentElement.textContent = myComment;
 
 const handleInput = debounce((e: any) => {
   localStorage.setItem('my-comment', myCommentElement.textContent!);
 }, 2000);
 myCommentElement.addEventListener('input', handleInput);
+
+myCommentElement.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+
+  myCommentContextMenu.popup();
+});
+
+const postCommentElement = document.querySelector('.post-comment') as HTMLParagraphElement;
+
+postCommentElement.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+
+  postCommentContextMenu.popup();
+});
 
 function render(post?: Post): void {
   document.querySelector('.post-comment')!.innerHTML = post ? post.message : '';
