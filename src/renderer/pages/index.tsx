c7083f2942c4ce46230d9b3399294afcc942c620
rikuba <rikuba@live.jp>
2017-05-23 23:52:25 +0900

    feat: 終了時に開いていたURLを次回起動時に復元する

--- src/renderer/pages/index.tsx
+++ src/renderer/pages/index.tsx
@@ -6,7 +6,7 @@ import { AppContainer } from 'react-hot-loader';
 
 import App from '../components/app';
 import configureStore from '../store';
-import { subWindowClosed } from '../actions';
+import { openThread, subWindowClosed } from '../actions';
 
 import './index.css';
 
@@ -31,4 +31,25 @@ if (module.hot) {
 
 ipcRenderer.on('sub-window-closed', () => {
   store.dispatch(subWindowClosed());
+});
+
+
+let lastThreads: string[] | null = null;
+
+store.subscribe(() => {
+  const state = store.getState();
+  const threads = state.threads.all;
+  if (threads === lastThreads) {
+    return;
+  }
+  lastThreads = threads;
+  localStorage.setItem('threads.all', JSON.stringify(threads));
+});
+
+
+const threadsJson = localStorage.getItem('threads.all');
+const threads: string[] = threadsJson ? JSON.parse(threadsJson) : [];
+
+threads.forEach((url) => {
+  store.dispatch(openThread(url));
 });
