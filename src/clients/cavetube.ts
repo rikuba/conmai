90d1ef546ab97c0a753f78b09925b02b51caba93
rikuba <rikuba@live.jp>
2017-06-15 17:41:07 +0900

    refactor: preload script内のロジックをclientに寄せる

--- src/clients/cavetube.ts
+++ src/clients/cavetube.ts
@@ -79,3 +79,36 @@ function detectStreamName(html: string): string {
   }
   return match[1];
 }
+
+
+export function collectPostData(comment: HTMLElement): Post {
+  const number = parseInt(comment.querySelector('.comment_number')!.textContent!, 10);
+
+  const nameElm = comment.querySelector('.name')!;
+  const author: Post['author'] = {
+    name: nameElm.textContent!,
+    url: null,
+    iconUrl: null,
+  };
+
+  const nameAnchor = nameElm.querySelector('a');
+  if (nameAnchor) {
+    author.url = nameAnchor.href;
+  }
+
+  const iconImg = comment.querySelector('.user_icon img');
+  if (iconImg) {
+    author.iconUrl = (iconImg as HTMLImageElement).src;
+  }
+
+  const postedAt = Date.parse(comment.querySelector('.post_time')!.getAttribute('datetime')!);
+
+  const message = comment.querySelector('.message')!.textContent!;
+
+  return {
+    number,
+    author,
+    postedAt,
+    message,
+  };
+}
--- src/scripts/cavetube.ts
+++ src/scripts/cavetube.ts
@@ -1,6 +1,6 @@
 import { ipcRenderer } from 'electron';
 
-import { Post } from '../clients/cavetube';
+import { collectPostData } from '../clients/cavetube';
 
 
 document.addEventListener('DOMContentLoaded', (e) => {
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
       return;
     }
 
-    const comments = (addedNodes as HTMLElement[]).map(collectPosts);
+    const comments = (addedNodes as HTMLElement[]).map(collectPostData);
     ipcRenderer.sendToHost('NEW_POSTS', comments);
   });
 
@@ -28,36 +28,3 @@ document.addEventListener('DOMContentLoaded', (e) => {
     childList: true,
   });
 });
-
-
-function collectPosts(comment: HTMLElement): Post {
-  const number = parseInt(comment.querySelector('.comment_number')!.textContent!, 10);
-
-  const nameElm = comment.querySelector('.name')!;
-  const author: Post['author'] = {
-    name: nameElm.textContent!,
-    url: null,
-    iconUrl: null,
-  };
-
-  const nameAnchor = nameElm.querySelector('a');
-  if (nameAnchor) {
-    author.url = nameAnchor.href;
-  }
-
-  const iconImg = comment.querySelector('.user_icon img');
-  if (iconImg) {
-    author.iconUrl = (iconImg as HTMLImageElement).src;
-  }
-
-  const postedAt = Date.parse(comment.querySelector('.post_time')!.getAttribute('datetime')!);
-
-  const message = comment.querySelector('.message')!.textContent!;
-
-  return {
-    number,
-    author,
-    postedAt,
-    message,
-  };
-}
