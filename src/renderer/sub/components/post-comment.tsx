bdc6c0931fe5c22a04f66f36cad30a14fcf4c488
rikuba <rikuba@live.jp>
2017-06-17 16:52:48 +0900

    feat: CaveTubeのコメント字幕に投稿者名を併せて表示する

--- src/renderer/sub/components/post-comment.tsx
+++ src/renderer/sub/components/post-comment.tsx
@@ -1,11 +1,55 @@
-import { ipcRenderer, remote } from 'electron';
+import { ipcRenderer } from 'electron';
 import React from 'react';
 
 import { setInnerHTML } from '../../../utils';
-import { Post } from '../../index/reducers';
+import { Post as ShitarabaPost } from '../../../clients/shitaraba-client';
+import { Post as CavetubePost } from '../../../clients/cavetube';
+import { Post as TwitchPost } from '../../../clients/twitch';
+
+
+type Post = 
+  { type: 'shitaraba' } & ShitarabaPost |
+  { type: 'cavetube'} & CavetubePost |
+  { type: 'twitch' } & TwitchPost;
+
+
+function ShitarabaPostComponent({ message }: ShitarabaPost) {
+  return (
+    <p className="sub post-comment"
+      ref={setInnerHTML(message)}
+    ></p>
+  );
+}
+
+function CavetubePostComponent({ author, message }: CavetubePost) {
+  let authorElm: React.ReactNode = null;
+  if (author.name) {
+    authorElm = author.name;
+    if (author.url) {
+      authorElm = <a href={author.url}>{authorElm}</a>;
+    }
+    authorElm = <span className="author">{authorElm}</span>;
+  }
+
+  return (
+    <div className="sub post-comment">
+      {authorElm}
+      <span className="message">{message}</span>
+    </div>
+  );
+}
+
+function TwitchPostComponent({ message }: TwitchPost) {
+  return (
+    <p className="sub post-comment">
+      {message}
+    </p>
+  );
+}
+
 
 type OwnState = {
-  text: string;
+  post: Post | null;
 };
 
 export default class PostComment extends React.Component<{}, OwnState> {
@@ -13,14 +57,13 @@ export default class PostComment extends React.Component<{}, OwnState> {
   private consumeTime = 10 * 1000;
   private timerId: any = 0;
 
-  state = {
-    text: '',
+  state: OwnState = {
+    post: null,
   };
 
   renderLoop = () => {
-    const post = this.posts.shift();
-    const text = post ? post.message : '';
-    this.setState({ text });
+    const post = this.posts.shift() || null;
+    this.setState({ post });
 
     const time = post ? this.consumeTime / (this.posts.length + 1) : 1000;
     this.timerId = setTimeout(this.renderLoop, time);
@@ -40,25 +83,22 @@ export default class PostComment extends React.Component<{}, OwnState> {
     clearTimeout(this.timerId);
   }
 
-  private contextMenu = remote.Menu.buildFromTemplate([
-    { role: 'copy', label: 'コピー' },
-    { role: 'selectall', label: 'すべて選択' },
-  ]);
-
-  handleContextMenu: React.MouseEventHandler<HTMLParagraphElement> = (e) => {
-    e.preventDefault();
-
-    this.contextMenu.popup();
-  };
-
   render() {
-    const { text } = this.state;
+    const { post } = this.state;
 
-    return (
-      <p className="sub post-comment"
-        onContextMenu={this.handleContextMenu}
-        ref={setInnerHTML(text)}
-      ></p>
-    );
+    if (post == null) {
+      return <div />;
+    }
+
+    switch (post.type) {
+      case 'shitaraba':
+        return <ShitarabaPostComponent {...(post as ShitarabaPost)} />;
+
+      case 'cavetube':
+        return <CavetubePostComponent {...(post as CavetubePost)} />;
+
+      case 'twitch':
+        return <TwitchPostComponent {...(post as TwitchPost)} />;
+    }
   }
 }
