95e914fef12b4c79e0ccd022a375ddf7812cf525
rikuba <rikuba@live.jp>
2018-02-01 22:13:40 +0900

    Format reducer

--- src/renderer/index/reducers/index.ts
+++ src/renderer/index/reducers/index.ts
@@ -8,21 +8,14 @@ import ui, { UI } from './ui';
 import pages, { Pages } from './pages';
 import { Page } from './page';
 
-export {
-  Preferences,
-  Posts,
-  Post,
-  Thread,
-  Pages,
-  Page,
-};
+export { Preferences, Posts, Post, Thread, Pages, Page };
 
 export interface State {
   preferences: Preferences;
   threads: Threads;
   posts: Posts;
   ui: UI;
-  pages: Pages,
+  pages: Pages;
 }
 
 export default combineReducers<State>({
